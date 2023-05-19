7aa1daf26c46d88a7126fca49f2d01234fa06159
your_name_here <sagarwale84@gmail.com>
2023-05-19 23:40:25 +0530

    Information is ongoing

--- Script/Information.js
+++ Script/Information.js
@@ -5,3 +5,13 @@ document.getElementById("next-button").addEventListener("click",next);
 function next(){
   window.location.href="Delivery.html"
 }
+
+
+// let apply=document.getElementById("apply");
+
+// apply.addEventListener("click",Apply());
+
+function Apply(){
+
+  document.getElementById("apply").innerText="Applied";
+}
